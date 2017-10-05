74cff2691b580593f87129f473abaaac6e6ef25e
MaxCreeger <bertranddatas@yahoo.fr>
2017-10-05 22:59:46 +0200

    branchement de la 1ere requete vers Elastic

--- dart/ConsoService/bin/consoservice.dart
+++ dart/ConsoService/bin/consoservice.dart
@@ -1,24 +1,40 @@
 import 'dart:async';
 
 import 'dart:io';
-import 'package:angular/angular.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
-@Injectable()
+
+
 class ConsoService {
   static final _headers = {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*'
+//    'Access-Control-Allow-Origin': '*',
+//    'Access-Control-Allow-Methods': '*'
   };
-  static const _elasticUrl = 'api/heroes'; // URL to web API
-  final Client _http;
 
-  ConsoService(this._http);
+  static const String _elasticUrl = "http://nounours:9200/teleinfo/conso/_search";
+  static const String _elasticHost = "nounours"; // URL to web API
+  static const int _elasticPort = 9200;
+  static const String _elasticPath = "teleinfo/conso/_search";
+
+
+  static const String _requeteJson =
+      '{'
+      '"size": 1,'
+      '"sort": { "dateMesure": "desc"},'
+      '"query": {'
+      '"match_all": {}'
+      '}'
+      '}';
+  ConsoService();
 
   Future<String> getConsoDuMoment() async {
-  HttpRequest request = new HttpRequest();
-  request.
+//  _client.open('POST', _elasticHost, _elasticPort, _elasticPath);
+  var response = await http.post(_elasticUrl,headers: _headers,body: _requeteJson) ;
+    stdout.writeln(response.body);
+
+  }
+
     /*try {
 
     } catch (e){
