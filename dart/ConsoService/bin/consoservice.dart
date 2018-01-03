7f02015f5eb83a63953ed107b974410f746a6406
MaxCreeger <bertranddatas@yahoo.fr>
2018-01-03 22:50:19 +0100

    début appel pour la requete conso sur une période

--- dart/ConsoService/bin/consoservice.dart
+++ dart/ConsoService/bin/consoservice.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'dart:convert';
 import 'dart:io';
+import 'lib/ConsoPeriodique.dart';
 import 'package:http/http.dart' as http;
 import 'lib/teleinfo.dart';
 import 'package:intl/intl.dart';
@@ -21,53 +22,89 @@ class ConsoService {
   static const int _elasticPort = 9200;
   static const String _elasticPath = "teleinfo/conso/_search";
 
-  static const String _requeteJson = '{'
+  static const String _requeteJsonConsoInstant = '{'
       '"size": 1,'
       '"sort": { "dateMesure": "desc"},'
       '"query": {'
       '"match_all": {}'
       '}'
       '}';
+
+  static const String _requeteJsonConso = '{'
+    '"query": {'
+      '"bool": {'
+        '"filter": {'
+          '"range": {'
+            '"dateMesure": {'
+              '"gte": "24/09/2017-22:15",'
+              '"lt": "24/09/2017-22:17",'
+              '"format": "dd/MM/yyyy-HH:mm"'
+            '}'
+          '}'
+        '}'
+      '}'
+    '}'
+  '}';
+
   ConsoService();
 
-  Future<String> getConsoDuMoment() async {
+  Future<Teleinfo> getConsoDuMoment() async {
 //  _client.open('POST', _elasticHost, _elasticPort, _elasticPath);
     var response =
-        await http.post(_elasticUrl, headers: _headers, body: _requeteJson);
+        await http.post(_elasticUrl, headers: _headers, body: _requeteJsonConsoInstant);
     stdout.writeln(response.body);
+    Teleinfo uneInfo = null;
     if (response.statusCode == 200) {
-      Map jsonData = JSON.decode(response.body);
-      Map hits = jsonData['hits'];
-      var nbDocs = hits['total'];
-      if (nbDocs >= 1) {
-        List docsWithMeta = hits['hits'];
-        Map unDocWithMeta = docsWithMeta[0];
-        Map unDoc = unDocWithMeta['_source'];
-
-        DateFormat formatter = new DateFormat('dd/MM/yyyy-HH:mm');
-
-        Teleinfo uneInfo = new Teleinfo(
-            unDoc['Periode'],
-            int.parse(unDoc['IndexHCreuses']),
-            int.parse(unDoc['IndexHPleines']),
-            formatter.parse(unDoc['dateMesure']),
-            int.parse(unDoc['PuissanceApp']));
-        stdout.writeln("uneInfo");
-        stdout.writeln("Date Mesure : "+formatter.format(uneInfo.dateMesure));
-        stdout.writeln("Index HC : "+  uneInfo.indexHC.toString());
-        stdout.writeln("Index HP : "+  uneInfo.indexHP.toString());
-        stdout.writeln("Periode : "+uneInfo.periode);
-        stdout.writeln("Conso en cours : "+uneInfo.puissanceApp.toString());
-
-      }
+      uneInfo = parserReponseElastic(response);
     }
-//  favoriteNumber.value = jsonData['favoriteNumber'].toString();
-//  valueOfPi.value = jsonData['valueOfPi'].toString();
-//  horoscope.value = jsonData['horoscope'].toString();
-//  favOne.value = jsonData['favoriteThings'][0];
-//  favTwo.value = jsonData['favoriteThings'][1];
-//  favThree.value = jsonData['favoriteThings'][2];
+    return uneInfo;
   }
+
+  /**
+   * parse la réponse faite par elastic pour produire un objet Teleinfo
+   */
+  Teleinfo parserReponseElastic(http.Response response) {
+
+    Teleinfo uneInfo = null;
+    Map jsonData = JSON.decode(response.body);
+    Map hits = jsonData['hits'];
+    var nbDocs = hits['total'];
+    if (nbDocs >= 1) {
+      List docsWithMeta = hits['hits'];
+      Map unDocWithMeta = docsWithMeta[0];
+      Map unDoc = unDocWithMeta['_source'];
+
+      DateFormat formatter = new DateFormat('dd/MM/yyyy-HH:mm');
+
+      uneInfo = new Teleinfo(
+          unDoc['Periode'],
+          int.parse(unDoc['IndexHCreuses']),
+          int.parse(unDoc['IndexHPleines']),
+          formatter.parse(unDoc['dateMesure']),
+          int.parse(unDoc['PuissanceApp']));
+      stdout.writeln("uneInfo");
+      stdout.writeln("Date Mesure : "+formatter.format(uneInfo.dateMesure));
+      stdout.writeln("Index HC : "+  uneInfo.indexHC.toString());
+      stdout.writeln("Index HP : "+  uneInfo.indexHP.toString());
+      stdout.writeln("Periode : "+uneInfo.periode);
+      stdout.writeln("Conso en cours : "+uneInfo.puissanceApp.toString());
+
+    }
+    return uneInfo;
+  }
+Future<ConsoPeriodique> getConsoDeLaPeriode(DateTime debut,DateTime fin) async {
+
+    ConsoPeriodique uneConso = null;
+    var response =
+    await http.post(_elasticUrl, headers: _headers, body: _requeteJsonConso);
+    stdout.writeln(response.body);
+    Teleinfo uneInfo = null;
+    if (response.statusCode == 200) {
+      uneInfo = parserReponseElastic(response);
+    }
+
+    return uneConso;
+}
 
   /*try {
 
