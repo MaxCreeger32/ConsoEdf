d443c3ae2b86db4e79822b33919708a07e4fa266
MaxCreeger <bertranddatas@yahoo.fr>
2018-01-03 22:47:49 +0100

    création de l'objet pour la conso sur une période

---
+++ dart/ConsoService/bin/lib/ConsoPeriodique.dart
@@ -0,0 +1,17 @@
+library ConsoService.bin.lib.consoPeriodique;
+
+class ConsoPeriodique {
+   DateTime dateDebut;
+   DateTime dateFin;
+
+   int indexHCDebut;
+   int indexHCFin;
+   int consoHC;
+
+   int indexHPDebut;
+   int indexHPFin;
+   int consoHP;
+
+
+
+}
